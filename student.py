f1a779f3321bd92061412f025010153bd8586be1
Vinoth Chinnasamy <134133328+Vinoth-2810@users.noreply.github.com>
2023-10-31 19:25:53 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 import json
+import login_rule
 
 
 def get_Studentjson():
@@ -18,3 +19,41 @@ def write_Studentjson(data):
 def get_profile(roll):
     data = get_Studentjson()[roll]
     return data["email"].upper(), roll.upper(), data["bookBorrow"], data["bookReturn"], data["notReturn"], data["fines"]
+
+
+def updateborrow(roll, bookid, borrowid):
+    datakeys = list(get_Studentjson().keys())
+    if roll in datakeys:
+        data = get_Studentjson()
+        data[roll]["bookBorrow"] = data[roll]["bookBorrow"] + 1
+        data[roll]["notReturn"] = data[roll]["notReturn"] + 1
+        data[roll]["Books"].append(bookid)
+        data[roll]["BorrowID"].append(borrowid)
+        write_Studentjson(data)
+    else:
+        data = get_Studentjson()
+        newdata = {
+            "email": "-",
+            "bookBorrow": 1,
+            "bookReturn": 0,
+            "notReturn": 1,
+            "fines": 0.0,
+            "Books": [],
+            "BorrowID": []
+        }
+        newdata["email"] = roll + "@kce.ac.in"
+        newdata["Books"].append(bookid)
+        newdata["BorrowID"].append(borrowid)
+        data[roll] = newdata
+        write_Studentjson(data)
+        login_rule.add_auth(newdata["email"])
+
+
+def updatertrn(roll, fines):
+    datakeys = list(get_Studentjson().keys())
+    if roll in datakeys:
+        data = get_Studentjson()
+        data[roll]["bookReturn"] = data[roll]["bookReturn"] + 1
+        data[roll]["notReturn"] = data[roll]["notReturn"] - 1
+        data[roll]["fines"] = data[roll]["fines"] + fines
+        write_Studentjson(data)
